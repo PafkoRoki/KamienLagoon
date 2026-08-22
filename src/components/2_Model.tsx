0c8afbee9b18d2aa5d2fd25f7f428ce86cf5bfb0
PafkoRoki <pafko.roki@gmail.com>
2026-08-22 02:13:09 +0200

    feat: add RippleDistortion component and new image asset for enhanced visual effects

--- src/components/2_Model.tsx
+++ src/components/2_Model.tsx
@@ -1,3 +1,4 @@
+import RippleDistortion from './RippleDistortion';
 import "./2_Model.css";
 
 export default function Model() {
@@ -28,6 +29,30 @@ export default function Model() {
             />
           </div>
         </div>
+
+            <div>
+              <RippleDistortion
+                src="https://raw.githubusercontent.com/PafkoRoki/KamienLagoon/main/public/images/Wiz5.jpg"
+                brushSize={150}
+                strength={0.2}
+                swirl={1}
+                rings={4}
+                grayscale
+                spread={5}
+                fade={3}
+                spacing={15}
+                dispersion={0}
+                glint={0}
+                tint="#a855f7"
+                tintAmount={0.1}
+                highlightColor="#ffffff"
+                trigger="hover"
+                clickStrength={2}
+                quality="low"
+                enabled
+            />
+            </div>
+
       </div>
     </section>
   );
